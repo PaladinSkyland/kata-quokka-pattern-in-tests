2952099173429855da965111f8c049458ead5a16
Hugo Parmentier <hugo.parmentier@efrei.net>
2024-11-21 10:56:21 +0100

    add default user in confest

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from shop import Address
+from shop import Address, User
 
 
 @pytest.fixture
@@ -10,3 +10,13 @@ def fsf_address():
 @pytest.fixture
 def paris_address():
     return Address("33 quai d'Orsay", "", "Paris", "75007", "France")
+
+@pytest.fixture
+def default_user():
+    return User(
+        name="bob",
+        email="bob@domain.tld",
+        age=25,
+        address=Address("51 Franklin Street", "Fifth Floor", "Boston", "02110", "USA"),
+        verified=True,
+    )
--- python/tests/test_shop.py
+++ python/tests/test_shop.py
@@ -1,50 +1,28 @@
 from shop import Shop, User
 
-
-def test_happy_path(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=25,
-        address=fsf_address,
-        verified=True,
-    )
+def test_happy_path(default_user):
+    user = default_user
 
     assert Shop.can_order(user)
     assert not Shop.must_pay_foreign_fee(user)
 
 
-def test_minors_cannot_order_from_the_shop(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=16,
-        address=fsf_address,
-        verified=True,
-    )
+def test_minors_cannot_order_from_the_shop(default_user):
+    user = default_user
+    user.age = 17
 
     assert not Shop.can_order(user)
 
 
-def test_cannot_order_if_not_verified(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=21,
-        address=fsf_address,
-        verified=False,
-    )
+def test_cannot_order_if_not_verified(default_user):
+    user = default_user
+    user.verified = False
 
     assert not Shop.can_order(user)
 
 
-def test_foreigners_must_be_foreign_fee(paris_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=25,
-        address=paris_address,
-        verified=False,
-    )
+def test_foreigners_must_be_foreign_fee(default_user, paris_address):
+    user = default_user
+    user.address = paris_address
 
     assert Shop.must_pay_foreign_fee(user)
